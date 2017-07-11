f991507c5eacac87216b6f7af8d50c9397c8799d
sbuckleybonanno <sbuckleybonanno@gmail.com>
2017-07-11 14:59:09 -0700

    made test.py tidier

--- test.py
+++ test.py
@@ -9,20 +9,24 @@ import matplotlib.pyplot as plt
 depths = []
 probabilities = []
 
+# differences = []
+
 max_depth = 22
 
 closure = 0
-previous_closure = 0
+# previous_closure = 0
 
 for i in range(0, max_depth, 2):
     depths.append(i)
     a = parenthesisclosure.NodeTree(i)
-    previous_closure = closure
+    # previous_closure = closure
     closure = a.closure_probability() * 100
 
-    print(closure - previous_closure)
+    # differences.append(closure - previous_closure)
+    # print(closure - previous_closure)
     probabilities.append(closure)
 
 plt.plot(depths, probabilities)
 plt.axis([0, max_depth, 0, 100])
+# plt.plot(differences)
 plt.show()
